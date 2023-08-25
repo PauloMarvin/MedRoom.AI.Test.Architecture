31e00c8228dfc247546ed986f3c7b7a9094d57f9
PauloMarvin <paulo.marving@gmail.com>
2023-08-25 14:36:11 -0300

    feat(processors): adds class for text preprocessing

---
+++ src/processors/text_processor_utils.py
@@ -0,0 +1,124 @@
+from __future__ import annotations
+
+import re
+from typing import Any, Dict, List
+
+import spacy
+from spacy.matcher import Matcher
+
+
+class TextProcessorUtils:
+    nlp_object = spacy.load(
+        'pt_core_news_sm',
+        disable=['parser', 'ner', 'tagger'],
+    )
+    nlp_object.add_pipe('emoji', first=True)
+
+    @classmethod
+    def remove_sites_corpus(cls, corpus: List[str]) -> List[str]:
+        formatted_corpus = []
+        for text in corpus:
+            formatted_corpus.append(cls.__remove_sites(text))
+        return formatted_corpus
+
+    @classmethod
+    def remove_punctuation_corpus(cls, corpus: List[str]) -> List[str]:
+        formatted_corpus = []
+        for text in corpus:
+            formatted_corpus.append(cls.__remove_punctuation(text))
+        return formatted_corpus
+
+    @classmethod
+    def lower_text_corpus(cls, corpus: List[str]) -> List[str]:
+        formatted_corpus = []
+        for text in corpus:
+            formatted_corpus.append(cls.__lower_text(text))
+        return formatted_corpus
+
+    @classmethod
+    def remove_stopwords_corpus(cls, corpus: List[str]) -> List[str]:
+        formatted_corpus = []
+        for text in corpus:
+            formatted_corpus.append(cls.__remove_stopwords(text))
+        return formatted_corpus
+
+    @classmethod
+    def lemmatization_corpus(cls, corpus: List[str]) -> List[str]:
+        formatted_corpus = []
+        for text in corpus:
+            formatted_corpus.append(cls.__lemmatization(text))
+        return formatted_corpus
+
+    @classmethod
+    def replace_matches_corpus(
+        cls,
+        corpus: List[str],
+        patterns_dict: Dict[str, List[List[Dict[str, Any]]]],
+    ) -> List[str]:
+        formatted_corpus = []
+        for text in corpus:
+            formatted_corpus.append(cls.__replace_matches(text, patterns_dict))
+        return formatted_corpus
+
+    @staticmethod
+    def __remove_sites(raw_text: str) -> str:
+        formatted_text = raw_text
+        for pattern in [r'(?:https?://)?(?:www\.)?[\w-]+\.[\w.-]+[^\s]*']:
+            formatted_text = re.sub(pattern, '', formatted_text)
+        return ' '.join(formatted_text.split())
+
+    @classmethod
+    def __remove_punctuation(cls, raw_text: str) -> str:
+        doc = cls.nlp_object(raw_text)
+        formatted_text = ' '.join(
+            [token.text for token in doc if not token.is_punct],
+        )
+        return formatted_text
+
+    @staticmethod
+    def __lower_text(raw_text: str) -> str:
+        return raw_text.lower()
+
+    @classmethod
+    def __remove_stopwords(cls, raw_text: str) -> str:
+        doc = cls.nlp_object(raw_text)
+        formatted_text = [token.text for token in doc if not token.is_stop]
+        return ' '.join(formatted_text)
+
+    @classmethod
+    def __lemmatization(cls, raw_text: str) -> str:
+        doc = cls.nlp_object(raw_text)
+        formatted_text = [token.lemma_ for token in doc]
+        return ' '.join(formatted_text)
+
+    @classmethod
+    def __replace_matches(
+        cls,
+        raw_text: str,
+        patterns_dict: Dict[str, List[List[Dict[str, Any]]]],
+    ) -> str:
+        patterns_dict = cls.__create_patterns_dict(patterns_dict)
+        matcher = Matcher(cls.nlp_object.vocab)
+        doc = cls.nlp_object(raw_text)
+        for key, value in patterns_dict.items():
+            matcher.add(key, value)
+        parsed_doc = doc.text
+        for match_id, start, end in matcher(doc):
+            string_id = cls.nlp_object.vocab.strings[match_id]
+            span = doc[start:end]
+            parsed_doc = parsed_doc.replace(span.text, string_id)
+
+        return parsed_doc
+
+    @classmethod
+    def __create_patterns_dict(
+        cls,
+        patterns_data: Dict[str, List[List[Dict[str, Any]]]],
+    ) -> Dict[str, List[List[Dict[str, Any]]]]:
+        patterns_dict = {}
+
+        for pattern_name, emojis in patterns_data.items():
+            pattern_list = [[{'ORTH': emoji}] for emoji in emojis]
+            patterns_dict[pattern_name] = pattern_list
+
+        return patterns_dict
